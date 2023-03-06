bbcea5ddecd08feec4e35c084011248fdb512992
Nickolas Solley <nicksolley@gmail.com>
2023-03-06 09:49:42 -0600

    student test

--- src/test/StudentTest.java
+++ src/test/StudentTest.java
@@ -1,12 +1,34 @@
-package test;
+import org.junit.Before;
+import org.junit.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StudentTest {
+    private Student student;
 
-    public static void main(String[] args) {
-        Student student = new Student("Nick");
-        student.addGrade(56.3);
-        student.addGrade(34.7);
-        student.addGrade(36.9);
-        System.out.println(student.getName() + "'s average grade is " + student.getGradeAverage());
+    @Before
+    public void setUp() {
+        student = new Student(1, "John Doe");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(1, student.getId());
+        assertEquals("John Doe", student.getName());
+        assertTrue(student.getGrades().isEmpty());
+    }
+
+    @Test
+    public void testAddGrade() {
+        student.addGrade(90);
+        assertEquals(90, (int) student.getGrades().get(0));
+    }
+
+    @Test
+    public void testGetGradeAverage() {
+        student.addGrade(90);
+        student.addGrade(80);
+        assertEquals(85.0, student.getGradeAverage(), 0.1);
     }
 }
